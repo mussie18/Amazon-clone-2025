452acd7f3b4a36cdd2061c776b1a173412e10fcf
mussie18 <mussiegebreslasie18@gmail.com>
2025-03-29 22:50:39 +0300

    Carousel

--- Amazon-clone/src/App.jsx
+++ Amazon-clone/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import CarouselEffect from "./Components/Carousel/Carousel";
 
 function App() {
   return (
     <>
       <Header />
+      <CarouselEffect />
     </>
   );
 }
